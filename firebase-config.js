57ce3a4cde6f42b3c8b5b79d307ec21522732392
LaithMazen <mazenlaith88@gmail.com>
2026-06-05 13:30:49 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,14 +5,14 @@
 // ============================================================
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDlS-rtXtG0rTKeH3HiYAyhhBjX_Z6HDqk",
-    authDomain: "rblxstudio-7abc8.firebaseapp.com",
-    projectId: "rblxstudio-7abc8",
-    storageBucket: "rblxstudio-7abc8.firebasestorage.app",
-    messagingSenderId: "430844890532",
-    appId: "1:430844890532:web:fc049907a303eb60da8531",
-    measurementId: "G-GPBERVMQL7"
-  };
+  apiKey: "AIzaSyDlS-rtXtG0rTKeH3HiYAyhhBjX_Z6HDqk",
+  authDomain: "rblxstudio-7abc8.firebaseapp.com",
+  projectId: "rblxstudio-7abc8",
+  storageBucket: "rblxstudio-7abc8.firebasestorage.app",
+  messagingSenderId: "430844890532",
+  appId: "1:430844890532:web:fc049907a303eb60da8531",
+  measurementId: "G-GPBERVMQL7"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
